f7edf06814256173728d905b2bc129c5e7dd445b
Abdelrhman Mohammed <abdelrhmanlearn@gmail.com>
2023-10-02 19:25:37 +0300

    added form with validation

--- noda_healthy_products/static/js/profile.js
+++ noda_healthy_products/static/js/profile.js
@@ -17,3 +17,24 @@ let removeOverlay = () => {
 overlay.addEventListener("click", removeOverlay)
 
 document.querySelector("#cancelLogout").addEventListener("click", removeOverlay)
+
+// VALIDATION
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+
+'use strict'
+
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+const forms = document.querySelectorAll('.needs-validation')
+
+// Loop over them and prevent submission
+Array.from(forms).forEach(form => {
+  form.addEventListener('submit', event => {
+    if (!form.checkValidity()) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    form.classList.add('was-validated')
+  }, false)
+})
